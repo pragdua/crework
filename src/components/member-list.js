40d31e34797c7f9d61701736a1b2ea26fe26ca2e
pragdua <to.pragundua@gmail.com>
2021-12-10 15:30:28 +0530

    Update member-list.js

--- src/components/member-list.js
+++ src/components/member-list.js
@@ -1,9 +1,9 @@
 export const members = [
     {name:"ISHANT JUYAL", role:"FOUNDER", desc:"Passionate about ed-tech and innovation." },
     {name:"ANTARIPA SAHA", role:"BATCH LEAD", desc:"Loves to build amazing things with text and voice" },
-    {name:"ARSHPREET SINGH", role:"COMMUNITY AND PRODUCT", desc:"One stop solution for everything from data to people" },
-    {name:"SAHIL SHARMA", role:"EX-FOUNDER + DEV", desc:"Can tell you what to code without looking at the screen" },
-    {name:"HERUMB SHANDILYA", role:"ML GAWD", desc:"Herumb can teach anything to anyone" },
-    {name:"AKSHAT GURNANI", role:"RESOURCE HUB", desc:"When we need to find resources, we call Akshat" },
     {name:"RUCHIKA PATRO", role:"COMMUNITY", desc:"This space isn't enough to list everything that she does" },
+    {name:"AKSHAT GURNANI", role:"RESOURCE HUB", desc:"When we need to find resources, we call Akshat" },
+    {name:"HERUMB SHANDILYA", role:"ML GAWD", desc:"Herumb can teach anything to anyone" },
+    {name:"SAHIL SHARMA", role:"EX-FOUNDER + DEV", desc:"Can tell you what to code without looking at the screen" },
+    {name:"ARSHPREET SINGH", role:"COMMUNITY AND PRODUCT", desc:"One stop solution for everything from data to people" },
 ]
